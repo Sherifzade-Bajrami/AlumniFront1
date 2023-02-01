ef7b0ddfdf7c3e5eaa6b9e68ea7cc92c32ee03e3
Ervin Osmani <eo51073@ubt-uni.net>
2023-02-01 16:45:14 +0100

    Events

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Home } from './views/Home';
 import { Students } from './views/Students';
+import { Events } from './views/Events';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,6 +14,7 @@ root.render(
   <Routes>
     <Route  path={'/'} element={<Home />} />
     <Route  path={'/students'} element={<Students />} />
+    <Route  path={'/events'} element={<Events />} />
     <Route />
     <Route />
     
---
+++ src/views/Events.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const Events = ()=>{
+    return<>
+        <header>
+            <ul>
+                <li>Home</li>
+                <li>Students</li>
+                <li>Events</li>
+                <li>News</li>
+            </ul>
+        </header>
+        <main>
+            <h1>This is Events page</h1>
+        </main>
+        <footer>
+            2023
+        </footer>
+    </>
+}
