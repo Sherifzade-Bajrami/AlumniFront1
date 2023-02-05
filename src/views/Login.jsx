c083ff92ebe4fd96d07f5db49e46057ae9d65959
Sherifzade-Bajrami <sb51075@ubt-uni.net>
2023-02-06 00:52:17 +0100

    finished login

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -15,8 +15,8 @@ export const Login = ()=> {
     }
 
     return<>
-    <div style={{ display:"flex", alignItems:"center",flexDirection:"column"}}>
-       
+    <div style={{ display:"flex", alignItems:"center",flexDirection:"column", padding:"80px 0"}}>
+       <div style={{padding:"5rem",border:"1px solid black",borderRadius:"30px",backgroundColor:"rgb(240, 240, 240)"}}>
        <form style={{flexDirection:"column",display:"flex"}} onSubmit={handleSubmit}>
         
         <label style={{padding:"0.5rem 0",fontSize:"20px" }} htmlFor="email">Email</label>
@@ -25,11 +25,12 @@ export const Login = ()=> {
         <label style={{padding:"0.5rem 0",fontSize:"20px" }} htmlFor="password">Password</label>
         <input style={{width:"300px",height:"40px",borderRadius:"10px"}}  value={password} onChange={(e)=>setPassword(e.target.value)} type="password" placeholder="  **********" id="password" name="password" />
         
-        <br></br>
+       
         
-        <button style={{backgroundColor:"black",color:"white", width:"150px",height:"40px",borderRadius:"8px"}} type="submit">Log in</button>
-        <br></br>
+        <button style={{backgroundColor:"black",color:"white", width:"150px",height:"40px",borderRadius:"8px",margin:"40px 0 0 70px"}} type="submit">Log in</button>
+       
        </form>
+     </div>
      </div>
        
     </>
