141f1d1c862344677b28808401857593e8fedeb5
Sherifzade-Bajrami <sb51075@ubt-uni.net>
2023-02-07 12:45:53 +0100

    edited news

--- src/views/News.jsx
+++ src/views/News.jsx
@@ -12,44 +12,22 @@ export const News = ()=>{
        }}>
         <div >
         <img src={require('./img/news.png')} height={'300'} width={'300'} alt='' className="img-h"/>
-        <h1 style={{
-            paddingLeft:"20px"
-        }}
-        >Title
-        <span style={{paddingLeft:"200px"}}>data</span>
-        </h1>
-        <p style={{
-            width:"80%",
-            paddingLeft:"25px"
-        }}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque aliquam placeat
+        <h3 style={{paddingLeft:"20px"}}>Title
+        <span style={{paddingLeft:"220px"}}>data</span>
+        </h3>
+        <p style={{ width:"90%", paddingLeft:"25px" }}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque aliquam placeat
              voluptas nihil deserunt pariatur eligendi,</p>
         </div>
         <div >
         <img src={require('./img/news.png')} height={'300'} width={'300'} alt='' className="img-h"/>
-        <h1 style={{
-            paddingLeft:"20px"
-        }}
-        >Title
-        <span style={{paddingLeft:"200px"}}>data</span>
-        </h1>
-        <p style={{
-            width:"80%",
-            paddingLeft:"25px"
-        }}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque aliquam placeat
+        <h3 style={{paddingLeft:"20px"}}>Title<span style={{paddingLeft:"220px"}}>data</span></h3>
+         <p style={{width:"90%",paddingLeft:"25px"}}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque aliquam placeat
              voluptas nihil deserunt pariatur eligendi,</p>
         </div>
         <div >
         <img src={require('./img/news.png')} height={'300'} width={'300'} alt='' className="img-h"/>
-        <h1 style={{
-            paddingLeft:"20px"
-        }}
-        >Title
-        <span style={{paddingLeft:"200px"}}>data</span>
-        </h1>
-        <p style={{
-            width:"80%",
-            paddingLeft:"25px"
-        }}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque aliquam placeat
+        <h3 style={{paddingLeft:"20px"}}>Title<span style={{paddingLeft:"220px"}}>data</span></h3>
+        <p style={{width:"90%",paddingLeft:"25px"}}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque aliquam placeat
              voluptas nihil deserunt pariatur eligendi,</p>
         </div>
        </section>
